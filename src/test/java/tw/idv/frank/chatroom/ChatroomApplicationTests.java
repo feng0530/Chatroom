f011630af3e54ec283edd7cfba80d68935205c63
23001211JAMESWE\Frank <fff80705@gmail.com>
2024-04-25 15:02:25 +0800

    create project

---
+++ src/test/java/tw/idv/frank/chatroom/ChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.idv.frank.chatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
